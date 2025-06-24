7bb5d99a211ed5103f4a18643fb4f1684205a1f8
Deniz Altınışık <deniz.altnk06@gmail.com>
2025-06-24 12:00:09 +0300

    railway missing start command

--- backend/app.py
+++ backend/app.py
@@ -1588,4 +1588,5 @@ def update_feedback():
         return jsonify({'success': False, 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
